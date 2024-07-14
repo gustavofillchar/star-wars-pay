2b3c7676193d47ee977bacf5fdc7dee7a16945cf
Gustavo Lima <guslimadev@gmail.com>
2024-07-13 21:07:19 -0300

    implement testing

--- src/app/components/Nav/_components/Filter/filter-list.tsx
+++ src/app/components/Nav/_components/Filter/filter-list.tsx
@@ -9,19 +9,30 @@ type PlanetList = {
 
 export default function FilterList({ planets }: PlanetList) {
 
-    const {setFilter} = useFilterPlanet()
+    const { setFilter, filter } = useFilterPlanet()
 
     const hasResidentsPlanets = planets?.filter((planet: Planet) => planet.residents.length > 0)
 
+    function clearFilterSelected(){
+        setFilter('')
+    }
+
     return (
-        <div className="flex items-start gap-x-[12px]">
-            <label className="text-gray-gravity-300 font-normal text-[16px]">Filter By:</label>
-            <select autoComplete="false" onChange={e => setFilter(e.target.value)} className="filter-nav-select" >
-                <option value={''} defaultChecked>All</option>
-                {hasResidentsPlanets && hasResidentsPlanets.map((planet: any) =>
-                    <option key={planet.name} value={planet.url}>{planet.name}</option>
-                )}
-            </select>
+        <div className="w-full flex justify-between">
+            <div className="flex  flex-1 items-start gap-x-[12px]">
+                <label className="text-gray-gravity-300 font-normal text-[16px]">Filter By:</label>
+                <select value={filter} autoComplete="false" onChange={e => setFilter(e.target.value)} className="filter-nav-select" >
+                    <option value={''} defaultChecked>All</option>
+                    {hasResidentsPlanets && hasResidentsPlanets.map((planet: any) =>
+                        <option key={planet.name} value={planet.url}>{planet.name}</option>
+                    )}
+                </select>
+            </div>
+
+            <button onClick={() => clearFilterSelected()} disabled={!filter} className="uppercase hidden sm:flex justify-center items-center border h-[38px] px-[40px] text-sm border-blue-gravity text-blue-gravity disabled:text-gray-300 disabled:border-gray-gravity-50 disabled:bg-white hover:bg-blue-gravity hover:text-white transition-all duration-500">
+                Clear all
+            </button>
+
         </div>
     )
 }
--- src/app/components/Nav/navbar.tsx
+++ src/app/components/Nav/navbar.tsx
@@ -12,7 +12,7 @@ async function fetchAllPlanets(url: string): Promise<Planet[]> {
     let nextUrl: string | null = url;
 
     while (nextUrl) {
-        const response:any = await api.get<PlanetResponse>(nextUrl);
+        const response: any = await api.get<PlanetResponse>(nextUrl);
         const data = response.data;
         if (data.results) {
             planets = planets.concat(data.results);
@@ -25,11 +25,11 @@ async function fetchAllPlanets(url: string): Promise<Planet[]> {
 
 export default async function Nav() {
     const allPlanets = await fetchAllPlanets('https://swapi.dev/api/planets');
-  
+
     return (
         <nav className="sm:border-b">
             <div className="py-[16px] sm:py-[26px] px-[25px] sm:px-[50px] max-w-[1820px] min-[1920px]:mx-auto min-[1920px]:px-0">
-            <FilterList planets={allPlanets} />
+                <FilterList planets={allPlanets} />
             </div>
         </nav>
     )
